5bfdb1ebc9e392957201d006a39b2cc44ee0211d
hjy628 <hjy628@gmail.com>
2018-01-04 14:18:18 +0800

    设计模式之间关系及类之间关系

--- src/main/java/com/hjy/mtpattern/PackageInfo.java
+++ src/main/java/com/hjy/mtpattern/PackageInfo.java
@@ -4,4 +4,29 @@ package com.hjy.mtpattern;
  * Created by hjy on 17-12-4.
  */
 public class PackageInfo {
+    /**
+     * 设计模式之间的关系可以概括为:支持、变体、组合、备选这四种关系
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
+
+
+
+
+
+
+
+
+
+
+
 }
